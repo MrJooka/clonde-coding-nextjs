1237eae1b69142febd3c2dd444f2823c7c99a639
Jooka <mr.jooka@gmail.com>
2021-10-11 02:48:33 +0900

    2.1.4 라우터 객체를 이용하여 라우팅 하기

--- pages/vegetable/[name].jsx
+++ pages/vegetable/[name].jsx
@@ -2,11 +2,10 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 const name = () => {
-  const router = useRouter();
-  console.log(router);
+  const { query } = useRouter();
   return (
     <div>
-      <h2>Hello</h2>
+      <h2>Hello {query.name}</h2>
       <Link href="/">
         <a>Move to './'</a>
       </Link>
