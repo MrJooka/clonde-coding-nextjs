c557e08d66c19fb1db387db4d720fbf4d85bb6ed
Jooka <mr.jooka@gmail.com>
2021-10-11 03:15:28 +0900

    2.3.1 getServerSideProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,21 @@
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-
-const App = () => {
-  const [name, setName] = useState('');
-  const router = useRouter();
-
-  return (
-    <div>
-      <button type="button" onClick={() => router.push('/tomato')}>
-        tomato로 가기
-      </button>
-      <p>이름</p>
-      <input value={name} onChange={(e) => setName(e.target.value)} style={{ marginRight: '12px' }} />
-      <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
-        {name}으로 가기
-      </button>
-    </div>
-  );
+const index = ({ user }) => {
+  const username = user && user.name;
+  return <div>{username}</div>;
 };
 
-export default App;
+export const getServerSideProps = async () => {
+  try {
+    const res = await fetch('https://api.github.com/users/jerrynim');
+    console.log(res);
+    if (res.status === 200) {
+      const user = await res.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+};
+
+export default index;
